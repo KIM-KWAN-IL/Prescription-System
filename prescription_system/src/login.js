b776f8f17ded63f905c54723ffd24f5a3ed28b98
KIM-KWAN-IL <118953934+KIM-KWAN-IL@users.noreply.github.com>
2023-04-03 11:47:58 +0900

    Create login.js
    미완성

---
+++ prescription_system/src/login.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Login=()=>{
+    return(
+
+        <>
+        <h2 className="Name">웹이름</h2>
+        <div className='LoginBox'>
+            
+        
+                <form>
+                    <label htmlFor="username" style={{ marginLeft: 10, padding: 10}}>Username</label>
+                    <input type="text" id="username" />
+
+                    <label htmlFor="password" style={{ marginLeft: 10, padding: 10}}>Password</label>
+                    <input type="password"  id="password" />
+                    
+                    <div>
+                    <button type="submit" className="LoginButton">Login</button>
+                    </div>
+                    
+                </form>
+            </div></>
+    );
+}
+export default Login;
+    
